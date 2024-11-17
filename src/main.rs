98df305a7242a76a22de6d21250cf4ae86136e40
Jérôme Ramette <jerome.ramette@gmail.com>
2024-11-17 19:01:36 +0100

    ✨ Add CLI arguments management with clap
    - Add configuration for server port and host
    - Add configuration file path argument
    - Implement environment variables support

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
 use crate::logger::setup_logger;
-
 use anyhow::{Context, Result};
+use clap::Parser;
 use log::info;
 use mochi::setup_app;
-use std::env;
-use std::net::SocketAddr;
+use std::net::{IpAddr, SocketAddr};
 use tokio::net::TcpListener;
 
 mod logger;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to configuration file
+    #[clap(long, short, env = "CONFIG_PATH", default_value = "./config")]
+    config_path: String,
+
+    /// Port to listen on
+    #[clap(long, short, env = "PORT", default_value = "3000")]
+    port: u16,
+
+    /// IPv4 address to bind to
+    #[clap(long, short, env = "IP_ADDR", default_value = "0.0.0.0")]
+    ip_addr: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    setup_logger().context("Setup logger")?;
+    let args = Args::parse();
 
+    setup_logger().context("Setup logger")?;
     info!("Starting Mochi!");
 
-    let configpath = env::var("CONFIG_PATH").unwrap_or("./config".to_string());
-
-    let app = setup_app(configpath)?;
-
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let app = setup_app(args.config_path)?;
+    let ip: IpAddr = args.ip_addr.parse().context("Invalid IP address")?;
+    let addr = SocketAddr::new(ip, args.port);
     let tcp_listener = TcpListener::bind(addr).await?;
+
+    info!("Listening on: {}", addr);
 
     axum::serve(tcp_listener, app.into_make_service())
         .await
