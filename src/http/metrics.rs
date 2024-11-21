eb0eb40d6ea381167d5cc87aefba3caf0b492db5
Jérôme Ramette <jerome.ramette@gmail.com>
2024-11-21 19:13:30 +0100

    🚨 refactor optentelemetry custom metrics declaration

--- src/http/metrics.rs
+++ src/http/metrics.rs
@@ -1,21 +1,29 @@
-use opentelemetry::metrics::MeterProvider;
+use opentelemetry::global::meter_provider;
+use opentelemetry::metrics::Counter;
 use opentelemetry::KeyValue;
 
 #[derive(Clone)]
 pub struct MochiMetrics {
-    mochi_route_not_found_counter: opentelemetry::metrics::Counter<u64>,
-    mochi_proxy_request_counter: opentelemetry::metrics::Counter<u64>,
+    mochi_route_not_found_counter: Counter<u64>,
+    mochi_proxy_request_counter: Counter<u64>,
 }
 
 impl MochiMetrics {
     pub fn new() -> MochiMetrics {
-        let meter_provider = opentelemetry::global::meter_provider();
-
+        let meter_provider = meter_provider();
         let my_meter = meter_provider.meter("mochi");
 
         MochiMetrics {
-            mochi_route_not_found_counter: my_meter.u64_counter("mochi_route_not_found").init(),
-            mochi_proxy_request_counter: my_meter.u64_counter("mochi_proxy_request_counter").init(),
+            mochi_route_not_found_counter: my_meter
+                .u64_counter("mochi_route_not_found")
+                .with_description("Counter for routes not found")
+                .with_unit("1")
+                .build(),
+            mochi_proxy_request_counter: my_meter
+                .u64_counter("mochi_proxy_request_counter")
+                .with_description("Counter for proxy requests")
+                .with_unit("1")
+                .build(),
         }
     }
 
